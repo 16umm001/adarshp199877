cf4be672b0b98b3fdbc0e39ca246c11e01c7677b
Adars Pathak <adarshp199877@gmail.com>
2017-06-16 15:59:44 +0530

    improve java

---
+++ 1.java
@@ -0,0 +1,27 @@
+class s
+{
+	public static void main(String args[])
+
+{
+	int marks;
+	scanner sc=new scanner(system.in);
+	marks=sc.nextInte();
+
+	if(marks>=90)
+	{
+		system.out.printin("Grade s");
+	}
+	else if(marks>=70 && marks<=89)
+	{
+		sysytem.out.printin("Grade A");
+	}
+
+else if (marks>=50 && marks<=69)
+{
+
+	system.out.printin("Grade c");
+}
+
+}
+}
+
